b7ec8bb5097a4787ede4d71bc83892b2b58295f7
Kakanmain <dybabartosz@gmail.com>
2025-04-11 02:17:23 +0200

    Duzo zmian

--- Project_ppk2/Project_ppk2/klasy.h
+++ Project_ppk2/Project_ppk2/klasy.h
@@ -30,79 +30,194 @@ public:
 	}
 };*/
 
-class plan {	
-	int w = 44;
-	int k = 9;
-	std::string p[44][9];
+class plan{	
+	int w = 57;
+	int k = 16;
+	std::string tabela[57][16]; 
 public:
 	plan() { //tworzy szkielet planu
-		p[0][0] = "Lp.";
-		p[0][1] = "Godz.";
-		p[0][2] = "Poniedzialek";
-		p[0][3] = "Wtorek";
-		p[0][4] = "Sroda";
-		p[0][5] = "Czwartek";
-		p[0][6] = "Piatek";
-		p[0][7] = "Sobota";
-		p[0][8] = "Niedziela";
-		for (int i = 1; i <= 43; i++) {
-			p[i][0] = std::to_string(i);
+		tabela[0][0] = "Godz.";
+		tabela[0][2] = "      Poniedzialek";
+		tabela[0][4] = "          Wtorek";
+		tabela[0][6] = "          Sroda";
+		tabela[0][8] = "        Czwartek";
+		tabela[0][10] = "         Piatek";
+		tabela[0][12] = "          Sobota";
+		tabela[0][14] = "        Niedziela";
+		
+		//myslniki poziome
+		std::stringstream myslniki, myslinik0;
+		myslniki << std::setw(25) << std::setfill('-') << "-"; //to dla wierszy
+		myslinik0 << std::setw(6) << std::setfill('-') << "-"; //to dla godziny
+		
+		for (int j = 1; j < w; j+=5) {
+			tabela[j][0] = myslinik0.str();
+			for (int i = 1; i < k; i++) {
+				if (i % 2 == 1) {
+
+				}
+				else {
+					tabela[j][i] = myslniki.str();
+				}
+			}
 		}
+		// myslniki pionowe
+		for (int j = 1; j < k; j += 2) { //to dla |
+			for (int i = 0; i < w; i++) {
+				if (i == 1) {
+					tabela[1][j] = "-";
+				} else {
+					tabela[i][j] = "|";
+				}
+			}
+		}
+		
+		//zajmuje sie godzinami
 		int h = 8;
 		int m = 00;
-
-		for (int i = 1; i <= 43; i++) {
-			if (m == 60) {
+		for (int i = 2; i <= 55; i++) { 
+			if (m == 60) { // jak jest 60 minuta to zwieksza godzine o 1
 				h++;
 				m = 00;
+				i++;
 			}
 			
 			std::stringstream oss;
 			oss << std::setw(2) << std::setfill('0') << h << ":" << std::setw(2) << std::setfill('0') << m;
-			
 			if (m == 00) {
-				p[i][1] = oss.str();
+				tabela[i][0] = oss.str();
 			}
-			else {
-				p[i][1] = oss.str();
-			}
+			//else { //tu s¹ minuty(co 15)
+			//	tabela[i][0] = oss.str();
+			//}
 			m += 15;	
 		}
 	}
 
-	void pokaz() {
+	void pokaz_plan() {
+		std::cout << "Twoj plan lekcji:" << std::endl << std::endl;
 		for (int x = 0; x < w; x++) {
 			for (int y= 0; y < k; y++) {
-				std::cout << std::setw(15) << std::left << p[x][y];
-				if (y == k-1) {
-					std::cout << std::endl;
+				if (y == 0) {
+					std::cout << std::setw(6) << std::left << tabela[x][y]; //wyswietla godziny
+				}
+				else if (y % 2 == 1) { //wyswietla |
+					std::cout << tabela[x][y];
+					if (y == 15) {
+						std::cout << std::endl;
+					}
+				}
+				else {
+					std::cout << std::setw(25) << std::left << tabela[x][y]; //wyswietla komorki ogolnie
 				}
 			}
 		}
 			
 	}
 
-	~plan() {
-		std::cout << "Plan zosta³ usuniêty" << std::endl;
+	~plan() {};
+};
+
+class grupa {
+	int numer;
+	std::string* lista_uczniow = new std::string[20];
+
+public:
+	grupa(int numer) : numer(numer) {
+
+	};
+
+	~grupa() {};
+};
+
+
+class osoba {
+	std::string imie, nazwisko;
+public:
+	osoba(const std::string imie, const std::string nazwisko) : imie(imie), nazwisko(nazwisko){
+
 	}
+
+	void inicjaly() {
+		std::cout << std::endl;
+		std::cout << "Imie: " << imie << std::endl << "Nazwisko: " << nazwisko << std::endl;
+	}
+
+	~osoba() {};
 };
 
-class nauczyciel {
-	std::string imie, nazwisko, przedmiot_prowadzony;
+class uczen : public osoba{
+	int numer_gr;
+public:
+	uczen(const std::string imie, const std::string nazwisko, int numer_gr) : osoba(imie, nazwisko), numer_gr(numer_gr) {
+		
+	}
+
+	void pokaz_dane() {
+		osoba::inicjaly();
+		std::cout << "Grupa dziekanska: "<<numer_gr << std::endl;
+	}
+
+
+	~uczen() {};
 };
 
-class klasa {
-	std::string typ;
+class nauczyciel : public osoba {
+	std::string przedmiot_prowadzony;
+public:
+	nauczyciel(const std::string imie, const std::string nazwisko, const std::string przedmiot_prowadzony) : osoba(imie, nazwisko), przedmiot_prowadzony(przedmiot_prowadzony){
+
+	}
+
+	void pokaz_dane() {
+		osoba::inicjaly();
+		std::cout << "Przedmiot prowadzony: " << przedmiot_prowadzony << std::endl;
+	}
+
+	~nauczyciel() {};
+};
+
+class sala {
+	std::string typ; //wykladowa, laboratoiryjna, komputerowa, ogolna...
 	char numer;
+public:
+	sala(std::string typ, char numer) : typ(typ), numer(numer) {
+
+	}
+
+	void pokaz_dane() {
+		std::cout << "Numer sali: " << numer << std::endl;
+		std::cout << "Typ sali: " << typ << std::endl;	
+	}
+
+	~sala() {};
 };
 
 class lekcja {
-	std::string przedmiot, nauczyciel, klasa, dzien;
-	int klasa, lp;
-
+	std::string przedmiot, typ;
+	int parzystosc; //0 nie wazne, 1 parzysty dzien, 2 nieparzysty
+	sala* klasa;
+	nauczyciel* prowadzacy;
 public:
-	lekcja(std::string przedmiot, std::string nauczyciel, int klasa, int lp, std::string dzien) {
+	lekcja(std::string przedmiot, std::string typ, int parzystosc, sala* nr, nauczyciel* n) : przedmiot(przedmiot), typ(typ), parzystosc(parzystosc), prowadzacy(n), klasa(nr){
 
+	}
+
+	void pokaz_dane() {
+		std::cout << "-----------------" << std::endl;
+		std::cout << przedmiot << ":" << std::endl << "Typ: "<< typ << std::endl;
+		if (parzystosc == 0){
+		std::cout<<"Co tydzien" << std::endl;
+		}
+		else if (parzystosc == 1) {
+			std::cout << "Tylko parzyste tygodnie" << std::endl;
+		}else if(parzystosc == 2) {
+			std::cout << "Tylko nieparzyste tygodnie" << std::endl;
+		}
+		std::cout << "Prowadzacy: ";
+		prowadzacy->pokaz_dane();
+		klasa->pokaz_dane();
+		std::cout <<"-----------------"<< std::endl;
 	}
 };
 
--- Project_ppk2/Project_ppk2/main.cpp
+++ Project_ppk2/Project_ppk2/main.cpp
@@ -1,18 +1,145 @@
 /*
 Projekt: Plan lekcji
-Przy stworzeniu planu, tworzy siê ogólny zarys planu lekcji. (Dni, liczba mo¿liwych lekcji i godziny)
+Przy stworzeniu planu, tworzy sie ogolny zarys planu lekcji. (Dni, liczba mozliwych lekcji i godziny)
 Plan bedzie tez mial kazdy nauczyciel i sala.
 Plany bedzie mozna modfikowac i usuwac
-Plan mozna wyswietlic, co bedzie pokazywalo zajecia godziny itp.
-Po zapytaniu o przedmiot poka¿e kiedy jest w jakiej sali.
+Plan mozna wyswietlic, co bedzie pokazywalo zajecia, godziny, itp.
+Po zapytaniu o przedmiot pokaze kiedy jest w jakiej sali.
+Po zapytanu o sale wyswietli jakie zajecia i kiedy sa w tej sali
 */
 
 #include <iostream>
 #include "klasy.h"
 
-int main() {
-	plan plan1;
+struct Dane {
+	nauczyciel* n[18];
+	lekcja* l[18];
+	uczen* u[25];
+};
 
-	std::cout << "Twoj plan lekcji:" << std::endl;
-	plan1.pokaz();
+Dane inicjalizujDane() {
+	Dane d;
+
+	sala* s1 = new sala("ogolna", '3');
+
+	//Nauczyciele (imie, nazwisko, przedmiot)
+	d.n[0] = new nauczyciel("Justyna", "Juszczyk-Synowiec", "fizyka");
+	d.n[1] = new nauczyciel("Jerzy", "Bodzenta", "fizyka");
+	d.n[2] = new nauczyciel("Jacek", "Checinski", "elektronika i miernictwo");
+	d.n[3] = new nauczyciel("Krzysztof", "Bernacki", "elektronika i miernictwo");
+	d.n[4] = new nauczyciel("Ewa", "Lobos", "analiza matematyczna i algebra liniowa");
+	d.n[5] = new nauczyciel("Bartlomiej", "Pawlik", "analiza matematyczna i algebra liniowa");
+	d.n[6] = new nauczyciel("Jan", "Machniewski", "podstawy elektrotechniki");
+	d.n[7] = new nauczyciel("Andrzej", "Pulka", "podstawy elektrotechniki");
+	d.n[8] = new nauczyciel("Agnieszka", "Danek", "algorytmy i struktury danych");
+	d.n[9] = new nauczyciel("Zbigniew", "Czech", "algorytmy i struktury danych");
+	d.n[10] = new nauczyciel("Robert", "Tutajewicz", "podstawy informatyki");
+	d.n[11] = new nauczyciel("Roman", "Starosolski", "programowanie komputerow");
+	d.n[12] = new nauczyciel("Grzegorz", "Kwiatkowski", "programowanie komputerow");
+	d.n[13] = new nauczyciel("Tomasz", "Rudnicki", "teoria ukladow cyfrowych");
+	d.n[14] = new nauczyciel("Adam", "Opara", "teoria ukladow cyfrowych");
+	d.n[15] = new nauczyciel("Piotr", "Zemla", "wychowanie fizyczne");
+	d.n[16] = new nauczyciel("Beata", "Badowska-Ekiert", "jezyk angielski");
+
+	//Lekcje  (nazwa, typ, parzystosc, sala, nauczyciel)
+	d.l[0] = new lekcja("Fizyka", "CW", 0, s1, d.n[0]);
+	d.l[1] = new lekcja("Fizyka", "WYK", 0, s1, d.n[1]);
+	d.l[2] = new lekcja("Elektronika i miernictwo", "CW", 0, s1, d.n[2]);
+	d.l[3] = new lekcja("Elektronika i miernictwo", "WYK", 0, s1, d.n[3]);
+	d.l[4] = new lekcja("Analiza matematyczna i algebra liniowa", "CW", 0, s1, d.n[4]);
+	d.l[5] = new lekcja("Analiza matematyczna i algebra liniowa", "WYK", 0, s1, d.n[5]);
+	d.l[6] = new lekcja("Podstawy elektrotechniki", "CW", 1, s1, d.n[6]);
+	d.l[7] = new lekcja("Podstawy elektrotechniki", "WYK", 1, s1, d.n[7]);
+	d.l[8] = new lekcja("Algorytmy i struktury danych", "CW", 1, s1, d.n[8]);
+	d.l[9] = new lekcja("Algorytmy i struktury danych", "WYK", 1, s1, d.n[9]);
+	d.l[10] = new lekcja("Podstawy informatyki", "CW", 1, s1, d.n[10]);
+	d.l[11] = new lekcja("Podstawy informatyki", "WYK", 2, s1, d.n[11]);
+	d.l[12] = new lekcja("Programowanie komputerow", "CW", 0, s1, d.n[11]);
+	d.l[13] = new lekcja("Programowanie komputerow", "LAB", 2, s1, d.n[12]);
+	d.l[14] = new lekcja("Teoria ukladow cyfrowych", "CW", 2, s1, d.n[13]);
+	d.l[15] = new lekcja("Teoria ukladow cyfrowych", "WYK", 0, s1, d.n[14]);
+	d.l[16] = new lekcja("Wychowanie fizyczne", "CW", 0, s1, d.n[15]);
+	d.l[17] = new lekcja("Jezyk angielski (C1)", "LEKT", 0, s1, d.n[16]);
+
+	//Lekcje  (imie, nazwisko, gr dziekanska)
+	d.u[0] = new uczen("Andrzej", "Piaseczny", 3);
+	d.u[1] = new uczen("Katarzyna", "Nowicka", 1);
+	d.u[2] = new uczen("Michal", "Kowalewski", 4);
+	d.u[3] = new uczen("Patrycja", "Jablonska", 2);
+	d.u[4] = new uczen("Tomasz", "Stepien", 5);
+	d.u[5] = new uczen("Julia", "Wojcik", 6);
+	d.u[6] = new uczen("Sebastian", "Krawczyk", 3);
+	d.u[7] = new uczen("Aleksandra", "Piekarska", 2);
+	d.u[8] = new uczen("lukasz", "Baran", 4);
+	d.u[9] = new uczen ("Natalia", "Lis", 1);
+	d.u[10] = new uczen("Damian", "Mazur", 5);
+	d.u[11] = new uczen("Dominika", "Szymanska", 6);
+	d.u[12] = new uczen("Kacper", "Zawadzki", 2);
+	d.u[13] = new uczen("Maja", "Sikora", 3);
+	d.u[14] = new uczen("Piotr", "Blaszczyk", 4);
+	d.u[15] = new uczen("Amelia", "Rutkowska", 1);
+	d.u[16] = new uczen("Jan", "Kubiak", 6);
+	d.u[17] = new uczen("Zuzanna", "Czarnecka", 2);
+	d.u[18] = new uczen("Oliwia", "Kowalczyk", 3);
+	d.u[19] = new uczen("Adam", "Krol", 5);
+	d.u[20] = new uczen("Magdalena", "Pawlak", 4);
+	d.u[21] = new uczen("Bartlomiej", "Jasinski", 2);
+	d.u[22] = new uczen("Karolina", "Witkowska", 6);
+	d.u[23] = new uczen("Marcin", "Lesniak", 1);
+	d.u[24] = new uczen("Joanna", "Sawicka", 3);
+
+
+	return d;
+}
+
+void usunDane(Dane& d) {
+	for (int i = 0; i < 18; i++) {
+		delete d.l[i];
+	}
+	for (int i = 0; i < 17; i++) {
+		delete d.n[i];
+	}
+	for (int i = 0; i < 25; i++) {
+		delete d.u[i];
+	}
+}
+
+int main() {
+	int odp;
+	plan p1, p2, p3, p4, p5, p6;
+	Dane d = inicjalizujDane();
+
+	std::cout << "===Plan lekcji===" << std::endl;
+	std::cout << "1. Pokaz plan lekcji" << std::endl;
+	std::cout << "2. Pokaz dane ucznia" << std::endl;
+	std::cout << "3. Pokaz dane nauczyciela" << std::endl;
+	std::cout << "4. Pokaz dane lekcji" << std::endl;
+	std::cout << "5. Pokaz dane sali" << std::endl;
+	std::cout << "6. Wyjdz" << std::endl;
+	std::cout << "Wybierz opcje: ";
+	std::cin >> odp;
+	std::cout << std::endl;
+
+	if (odp == 1) {
+		p1.pokaz_plan();
+	} else if(odp == 2) {
+		d.u[5]->pokaz_dane();
+	}
+	else if (odp == 3) {
+		d.n[1]->pokaz_dane();
+	}
+	else if (odp == 4) {
+		d.l[10]->pokaz_dane();
+	}
+	else if (odp == 5) {
+		/*s1->pokaz_dane(); //nie dzia³a*/ 
+	}
+	else if (odp == 6) {
+		return 0;
+	}
+	else {
+		std::cout << "Bledna odpowiedz. Wybierz pomiedzy 1, a 6" << std::endl;
+	}
+
+	usunDane(d);
 }
